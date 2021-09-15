13d17f17172585d8f27c4804ec484a3013830da5
forcemain <forcemain@163.com>
2021-09-15 13:37:04 +0800

    :underage:

--- setup.py
+++ setup.py
@@ -18,6 +18,7 @@ setup(
     long_description=readme,
     long_description_content_type='text/markdown',
     packages=find_packages(exclude=['test', 'test.*']),
+    install_requires=['service-kombu'],
     classifiers=[
         'Typing :: Typed',
         'Operating System :: MacOS',
@@ -25,6 +26,5 @@ setup(
         'Operating System :: POSIX',
         'Programming Language :: Python',
         'Programming Language :: Python :: 3.9',
-    ],
-    install_requires=['flask==2.0.1', 'service-kombu']
+    ]
 )
